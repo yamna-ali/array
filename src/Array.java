21acf17a8e7ccedde20afbb4a216fc6902cfac63
yamna-ali <yamna_fasih.92@hotmail.com>
2020-08-27 12:01:27 -0400

    first commit

---
+++ src/Array.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array {
+public static void main(String[] args){
+    /*
+    Numbers: [1, 21, 63, 4, 51]
+Strings: [Sunday, Monday, Tuesday, Wednesday, Thursday, Fridat, Saturday]
+Characters:[a, e, i, o, u]
+Booleans:[true, false]
+
+     */
+    int number[]={1, 21, 63, 4, 51};
+ String[] strings={"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+  char  Characters[]={'a', 'e', 'i', 'o', 'u'};
+boolean Booleans[]={true, false};
+    System.out.println( "Numbers:" +Arrays.toString(number));
+    System.out.println("Strings: "+ Arrays.toString(strings));
+    System.out.println("Characters: "+ Arrays.toString(Characters));
+    System.out.println( "Booleans: "+Arrays.toString(Booleans));
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
